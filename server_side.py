8fc43ecbb6410ba3f6a9c47427c52ebf657e8f26
Jakub_Buler <buler.jakub@gmail.com>
2022-11-06 01:23:29 +0100

    init

--- server_side.py
+++ server_side.py
@@ -1,6 +1,3 @@
-# from PyQt5.QtGui import *
-# from PyQt5.QtWidgets import *
-# from PyQt5.QtCore import *
 from PyQt5.QtWidgets import QGridLayout, QPushButton, QLineEdit, QTextEdit
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QWidget, QMainWindow, QApplication
